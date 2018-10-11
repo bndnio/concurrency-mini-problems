a8911ff81704997fcacfc8be3c2eba7bb0894eb9
Brendon Earl <bearl@uvic.ca>
2018-10-11 08:53:40 -0700

    add profiling to H2O

--- H2O/H2O-buffered.go
+++ H2O/H2O-buffered.go
@@ -1,10 +1,17 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
 	"sync"
+	"flag"
+	"os"
+	"log"
+	"runtime"
+	"runtime/pprof"
 )
 
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
+var memprofile = flag.String("memprofile", "", "write memory profile to `file`")
 var wg sync.WaitGroup
 var hReady = make(chan bool, 2)
 var oReady = make(chan bool, 2)
@@ -12,7 +19,7 @@ var oReady = make(chan bool, 2)
 func H() {
 	hReady <- true
 	<- oReady
-	fmt.Println("H through")
+	// fmt.Println("H through")
 	wg.Done()
 }
 
@@ -21,18 +28,38 @@ func O() {
 	<- hReady
 	oReady <- true
 	oReady <- true
-	fmt.Println("O through")
+	// fmt.Println("O through")
 	wg.Done()
 }
 
 func main() {
-	for i:=0; i<20; i++ {
-		wg.Add(1)
+	flag.Parse()
+    if *cpuprofile != "" {
+        f, err := os.Create(*cpuprofile)
+        if err != nil {
+            log.Fatal(err)
+        }
+        pprof.StartCPUProfile(f)
+        defer pprof.StopCPUProfile()
+	}
+
+	for i:=0; i<1000000; i++ {
+		wg.Add(3)
+		go H()
+		go O()
 		go H()
 	}
-	for i:=0; i<10; i++ {
-		wg.Add(1)
-		go O()
-	}
 	wg.Wait()
+
+	if *memprofile != "" {
+        f, err := os.Create(*memprofile)
+        if err != nil {
+            log.Fatal("could not create memory profile: ", err)
+        }
+        runtime.GC() // get up-to-date statistics
+        if err := pprof.WriteHeapProfile(f); err != nil {
+            log.Fatal("could not write memory profile: ", err)
+        }
+        f.Close()
+    }
 }
