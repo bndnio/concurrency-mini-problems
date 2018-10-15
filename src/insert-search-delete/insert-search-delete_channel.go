2ade04cb5cbf59aa4536922b4c16a2b09f89d781
Brendon Earl <bearl@uvic.ca>
2018-10-15 01:16:14 -0700

    Add perf measurement utilities to insert-search-delete

--- src/insert-search-delete/insert-search-delete_channel.go
+++ src/insert-search-delete/insert-search-delete_channel.go
@@ -12,7 +12,15 @@ import (
 	"sync"
 	"container/list"
 	"lightswitch_channel"
+	"flag"
+	"os"
+	"log"
+	"runtime"
+	"runtime/pprof"
 )
+
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
+var memprofile = flag.String("memprofile", "", "write memory profile to `file`")
 
 var l *list.List = nil
 var wg sync.WaitGroup
@@ -68,6 +76,16 @@ func print() {
 }
 
 func main() {
+	flag.Parse()
+    if *cpuprofile != "" {
+        f, err := os.Create(*cpuprofile)
+        if err != nil {
+            log.Fatal(err)
+        }
+        pprof.StartCPUProfile(f)
+        defer pprof.StopCPUProfile()
+	}
+
 	insertMutex <- true
 	noSearcher <- true
 	noInserter <- true
@@ -89,4 +107,17 @@ func main() {
 	}
 
 	wg.Wait()
+
+	if *memprofile != "" {
+		runtime.MemProfileRate = 1
+        f, err := os.Create(*memprofile)
+        if err != nil {
+            log.Fatal("could not create memory profile: ", err)
+        }
+        runtime.GC() // get up-to-date statistics
+        if err := pprof.WriteHeapProfile(f); err != nil {
+            log.Fatal("could not write memory profile: ", err)
+        }
+        f.Close()
+    }
 }
