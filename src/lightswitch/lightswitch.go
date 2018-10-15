86c7cfd7c73e877b71d34f463f45270d0e24d852
Brendon Earl <bearl@uvic.ca>
2018-10-14 19:40:46 -0700

    Move and fix lightswitch to work!

---
+++ src/lightswitch/lightswitch.go
@@ -0,0 +1,38 @@
+package lightswitch
+
+import (
+	"fmt"
+	"sync"
+)
+
+type lightswitch struct {
+	mutex *sync.Mutex
+	counter int
+}
+
+var ls *lightswitch
+
+func New() *lightswitch {
+	ls = &lightswitch{&sync.Mutex{}, 0}
+	return ls
+}
+
+func (ls *lightswitch) Lock(m *sync.Mutex) {
+	ls.mutex.Lock()
+	defer ls.mutex.Unlock()
+	
+	ls.counter += 1
+	if ls.counter == 1 {
+		m.Lock()
+	}
+}
+
+func (ls *lightswitch) Unlock(m *sync.Mutex) {
+	ls.mutex.Lock()
+	defer ls.mutex.Unlock()
+	
+	ls.counter -= 1
+	if ls.counter == 0 {
+		m.Unlock()
+	}
+}
