d746c9d3da6b21b228e9c066552596a80523dc06
Brendon Earl <bearl@uvic.ca>
2018-10-14 19:59:25 -0700

    complete working insert-search-delete

--- src/insert-search-delete/insert-search-delete.go
+++ src/insert-search-delete/insert-search-delete.go
@@ -4,26 +4,27 @@ import (
 	"fmt"
 	"sync"
 	"container/list"
-	"./lightswitch"
+	"lightswitch"
 )
 
 var l *list.List = nil
 var wg sync.WaitGroup
 
-insertMutex = &sync.Mutex{}
-noSearcher = &sync.Mutex{}
-noInserter = &sync.Mutex{}
-searchSwitch = &lightswitch
-insertSwitch = &lightswitch
+var insertMutex = &sync.Mutex{}
+var noSearcher = &sync.Mutex{}
+var noInserter = &sync.Mutex{}
+var searchSwitch = lightswitch.New()
+var insertSwitch = lightswitch.New()
 
 func insert(value int) {
 	defer wg.Done()
+	searchSwitch.Lock(noSearcher)
+	defer searchSwitch.Unlock(noSearcher)
+
 	l.PushBack(value)
 }
 
-func search(value int) *list.Element {
-	defer wg.Done()
-	defer 
+func find(value int) *list.Element {
 	for e := l.Front(); e != nil; e = e.Next() {
 		if e.Value == value {
 			return e
@@ -32,10 +33,24 @@ func search(value int) *list.Element {
 	return nil
 }
 
+func search(value int) *list.Element {
+	defer wg.Done()
+	insertSwitch.Lock(noInserter)
+	insertMutex.Lock()
+	defer insertSwitch.Unlock(noInserter)
+	defer insertMutex.Unlock()
+
+	return find(value)
+}
+
 func delete(value int) {
 	defer wg.Done()
-	wg.Add(1)
-	l.Delete(search(value))
+	noSearcher.Lock()
+	noInserter.Lock()
+	defer noSearcher.Unlock()
+	defer noInserter.Unlock()
+
+	l.Remove(find(value))
 }
 
 func print() {
@@ -46,10 +61,21 @@ func print() {
 }
 
 func main() {
+
 	l = list.New()
-	for i:=0; i<20; i++ {
-		wg.Add(1)
-		insert(i)
+	for i:=0; i<100; i++ {
+		if i%2 == 0 {
+			wg.Add(1)
+			insert(i)
+		} else if i%3 == 0 {
+			wg.Add(10)
+			for j:=0; j<10; j++ {
+				search(i-1)
+			}
+		} else {
+			wg.Add(1)
+			delete(i-1)
+		}
 	}
 
 	wg.Wait()
